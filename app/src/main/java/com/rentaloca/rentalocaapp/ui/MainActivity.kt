1aa3e416089685e2c736deee41757cff12ae10d5
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-06-09 10:04:34 +0700

    delete order page

--- app/src/main/java/com/rentaloca/rentalocaapp/ui/MainActivity.kt
+++ app/src/main/java/com/rentaloca/rentalocaapp/ui/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_camera, R.id.navigation_order, R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_camera, R.id.navigation_profile
             )
         )
         // setupActionBarWithNavController(navController, appBarConfiguration)
